808a6351881041ea0eb0e56e614784a020d6b3a3
Protim Aich <protimaich2001@gmail.com>
2022-07-19 20:58:24 +0530

    First Commit

--- proshop/src/App.js
+++ proshop/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import  {Container} from 'react-bootstrap'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+        <Header/>
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen/>} exact />
+              <Route path='/product/:id' element={<ProductScreen/>}/>
+            </Routes>
+          </Container>
+        </main>
+        <Footer/>
+    </Router>
   );
 }
 
